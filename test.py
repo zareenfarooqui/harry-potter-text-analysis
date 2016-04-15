49213f00b841d1ecefdfbbac0752302ce5f63ecd
Zareen <zareenf@gmail.com>
2016-04-15 13:50:08 -0400

    Zareen's edits
    Edited line 3 and added a new line 5

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 # this is a test for adding conflicting code via seperate pull requests
 print "Hello World"
-print "This is the original code by Zareen"
+print "This is definitely the original code by Zareen" #zareen
 print "What will happen when we try to edit?"
+print "Zareen added this" #zareen
